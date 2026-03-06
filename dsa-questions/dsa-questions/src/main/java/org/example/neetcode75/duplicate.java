9d850039ffc440250119b79aca812bfdb8ce07a0
shivrajak270 <shivrajak280@gmail.com>
2026-03-07 01:42:21 +0530

    ghg

--- dsa-questions/dsa-questions/src/main/java/org/example/neetcode75/duplicate.java
+++ dsa-questions/dsa-questions/src/main/java/org/example/neetcode75/duplicate.java
@@ -1,4 +1,9 @@
 package org.example.neetcode75;
 
 public class duplicate {
+
+
+    public static void main(String[] args) {
+
+    }
 }
---
+++ neetcode 75/contains_duplicates.java
@@ -0,0 +1,17 @@
+class conatins_duplicates{
+
+    public String printg(){
+        System.out.print("Enter String: ");
+    }
+
+
+
+    public static void main(Strings args[]){
+         contains_duplicates dup=new conatins_duplicates();
+         dup.printg();
+
+
+
+
+    }
+}
